d2f7c5d624265d06cb8bb15c86d95558ee75e1f1
JHolt <kingdomseed@gmail.com>
2018-07-01 22:03:05 -0700

    Food is done.

--- app/src/main/java/com/holtnet/arlingtontourguide/FoodFragment.java
+++ app/src/main/java/com/holtnet/arlingtontourguide/FoodFragment.java
@@ -25,14 +25,10 @@ public class FoodFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.tour_guide_listview, container, false);
 
         locations = new ArrayList<>();
-        locations.add(new Location("Fork in the Road", "$$ | American | Lunch, Dinner", "Nestled in an old shopping center off Field Rd, Fork in the Road is a rare gem. Come for the Crackeroni, stay for the incredible sandwiches.!", R.drawable.forkinroad));
-        locations.add(new Location("Sydney", "$$$", "Duh!", R.drawable.forkinroad));
-        locations.add(new Location("Sydney", "$$$", "Duh!", R.drawable.forkinroad));
-        locations.add(new Location("Sydney", "$$$", "Duh!", R.drawable.forkinroad));
-        locations.add(new Location("Sydney", "$$$", "Duh!", R.drawable.forkinroad));
-        locations.add(new Location("Sydney", "$$$", "Duh!", R.drawable.forkinroad));
-        locations.add(new Location("Sydney", "$$$", "Duh!", R.drawable.forkinroad));
-
+        locations.add(new Location("Fork in the Road", "$$ | American | Lunch, Dinner", "Located in an old shopping center off Fielder Rd, Fork in the Road is a rare gem. Come for the Crackeroni, stay for the incredible sandwiches!", R.drawable.forkintheroad));
+        locations.add(new Location("Bethany Cafe", "$$ | Chinese | Lunch, Dinner", "Right off Cooper near UTA and Arlington High School is this gem. Incredible orange chicken and boba at a great price!!", R.drawable.bethany));
+        locations.add(new Location("Istanbull Grill", "$$$ | Mediterranean | Lunch, Dinner ", "The incredibly atmospheric Instanbull Grill is off South Cooper Street near Sublett. Try the Turkish Coffee!!", R.drawable.istanbulgrill));
+        locations.add(new Location("Old West Cafe", "$$ | American | Breakfast, Lunch", "An incredible pancake and breakfast house with a great brunch menu. Try the blueberry pancakes!!", R.drawable.oldwestcafe));
 
         LocationAdapter itemsAdapter = new LocationAdapter(getActivity(), locations);
 
